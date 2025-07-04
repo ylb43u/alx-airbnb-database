59e4857da53b54d4c781b828e57345c2b9d190bf
RachidJedata <jedatarachid@gmail.com>
2025-07-04 16:29:41 +0100

    feat:adding queries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,6 +5,6 @@ SELECT
 FROM Booking
 INNER JOIN User ON Booking.user_id = users.user_id;
 
-SELECT p.*,r.* FROM Property p LEFT JOIN Review r ON p.property_id = r.property_id;
+SELECT p.*,r.* FROM Property p LEFT JOIN Review r ON p.property_id = r.property_id ORDER BY p.property_id;
 
 SELECT u.*, b.* FROM User u FULL OUTER JOIN Booking b ON u.user_id = b.user_id;
